a3499e32b9e97e0b23bec113e27d07f477a03261
marcosbastos <marcosbastos@mpse.mp.br>
2023-01-26 10:59:49 -0300

    appsettings

--- VaultConsole/Program.cs
+++ VaultConsole/Program.cs
@@ -2,30 +2,43 @@
 using VaultSharp.V1.AuthMethods;
 using VaultSharp;
 using VaultSharp.V1.Commons;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-//conect
-IAuthMethodInfo authMethod = new TokenAuthMethodInfo(vaultToken: "myroot");
-VaultClientSettings vaultClientSettings = new
-VaultClientSettings("http://vault:8200", authMethod);
-IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 
-Console.WriteLine($"Conectou {DateTime.Now}");
+var vault_host = config.GetValue<string>("Vault:host");
+var vault_token = config.GetValue<string>("Vault:token");
+var vault_secret_path = config.GetValue<string>("Vault:secret_path");
 
-//write secret
-var secretData = new Dictionary<string, object> { { "ConnectionString", "mongodb://localhost:27017" } };
-vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync(
-    path: "/mongo",
-    data: secretData,
-    mountPoint: "secret"
-).Wait();
+try
+{
+    //conect
+    IAuthMethodInfo authMethod = new TokenAuthMethodInfo(vaultToken: vault_token);
+    VaultClientSettings vaultClientSettings = new
+    VaultClientSettings(vault_host, authMethod);
+    IVaultClient vaultClient = new VaultClient(vaultClientSettings);
 
-Console.WriteLine("Secret criado.");
+    //write secret
+    var secretData = new Dictionary<string, object> { { "ConnectionString", "mongodb://localhost:27017" } };
+    vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync(
+        path: vault_secret_path,
+        data: secretData,
+        mountPoint: "secret"
+    ).Wait();
+    Console.WriteLine($"Secret created - {DateTime.Now}");
 
-//read secret
-Secret<SecretData> secret = vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
-    path: "/mongo",
-    mountPoint: "secret"
-).Result;
-var conexao = secret.Data.Data["ConnectionString"];
-
-Console.WriteLine(conexao.ToString());
+    //read secret
+    Secret<SecretData> secret = vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+        path: vault_secret_path,
+        mountPoint: "secret"
+    ).Result;
+    var conexao = secret.Data.Data["ConnectionString"];
+    Console.WriteLine($"Secret: {conexao.ToString()}");
+}
+catch(Exception error)
+{
+    Console.WriteLine(error.ToString());
+}
